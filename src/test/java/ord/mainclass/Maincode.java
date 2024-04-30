5cda30c5604c08e057114bdfa368acfdbeaac894
Malavika126 <arunamalavika667@gmail.com>
2024-04-30 11:04:52 +0530

    code

--- src/test/java/ord/mainclass/Maincode.java
+++ src/test/java/ord/mainclass/Maincode.java
@@ -1,11 +1,13 @@
 package ord.mainclass;
 
+import org.apache.poi.util.SystemOutLogger;
 import org.testng.annotations.Test;
 
 public class Maincode {
 	@Test
 	private void test1() {
 		System.out.println("Open github");
+		System.out.println("Modifiy code");
 	}
 
 }
